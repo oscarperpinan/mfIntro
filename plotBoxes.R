2fe855b6323a604f1542c546c7dbbd778bd307d0
Oscar Perpiñán Lamigueiro <oscar@OPL-EUITI.upm.es>
2015-05-21 16:51:01 +0200

    OpenMeteo deja de funcionar.

--- plotBoxes.R
+++ plotBoxes.R
@@ -10,9 +10,6 @@ proj4string(pNAM) <- "+proj=longlat +datum=WGS84"
 pRAP <- as(mfExtent('rap'), 'SpatialPolygons')
 proj4string(pRAP) <- "+proj=longlat +datum=WGS84"
 
-pOM <- as(mfExtent('openmeteo'), 'SpatialPolygons')
-proj4string(pOM) <- "+proj=longlat +datum=WGS84"
-
 pMG4 <- as(mfExtent('meteogalicia', 4), 'SpatialPolygons')
 proj4string(pMG4) <- "+proj=longlat +datum=WGS84"
 
@@ -27,18 +24,16 @@ IDs <- sapply(strsplit(earth$names, ":"), function(x) x[1])
 earth <- map2SpatialLines(earth, IDs=IDs,
                              proj4string=CRS("+proj=longlat +datum=WGS84"))
 
-
-plot(earth, lwd = .2, xlim = c(-150, 55), ylim = c(10, 60))
+pdf('/tmp/mapaServices.pdf')
+plot(earth, lwd = .2, xlim = c(-150, 25), ylim = c(0, 60))
 plot(pNAM, add = TRUE)
 text(mfExtent('nam')@xmax + 3, mfExtent('nam')@ymin - 3,'NAM')
 plot(pRAP, add = TRUE)
 text(mfExtent('rap')@xmax + 3, mfExtent('rap')@ymin - 3, 'RAP')
-plot(pOM, add = TRUE)
-text(mfExtent('openmeteo')@xmax + 3, mfExtent('openmeteo')@ymin - 3, 'OM')
 plot(pMG4, add = TRUE)
 text(mfExtent('meteogalicia', 4)@xmax + 3, mfExtent('meteogalicia', 4)@ymin - 3, 'MG4')
 plot(pMG12, add = TRUE)
 text(mfExtent('meteogalicia', 12)@xmax + 3, mfExtent('meteogalicia', 12)@ymin - 3, 'MG12')
 plot(pMG36, add = TRUE)
 text(mfExtent('meteogalicia', 36)@xmax + 3, mfExtent('meteogalicia', 36)@ymin - 3, 'MG36')
-
+dev.off()
